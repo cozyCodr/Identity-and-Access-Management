e49d386397e23afff399a805e0b4d23c442e20db
Bright Londa <66696421+cozyCodr@users.noreply.github.com>
2022-10-03 07:57:51 +0200

    add app error handlers

--- Project/03_coffee_shop_full_stack/starter_code/backend/src/api.py
+++ Project/03_coffee_shop_full_stack/starter_code/backend/src/api.py
@@ -205,7 +205,25 @@ def unprocessable(error):
 '''
 
 
+@app.errorhandler(404)
+def resource_not_found(error):
+    return jsonify({
+        'success': False,
+        'error': 404,
+        'message': 'resource not found'
+    }), 404
+
+
 '''
 @TODO implement error handler for AuthError
     error handler should conform to general task above
 '''
+
+
+@app.errorhandler(AuthError)
+def unauthenticated(error):
+    return jsonify({
+        'success': False,
+        'error': error.status_code,
+        'message': error.error
+    })
