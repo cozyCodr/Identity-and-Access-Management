1e45d80fb3d3f1f1fffc6a31bdaa6c5209a0a471
Bright Londa <66696421+cozyCodr@users.noreply.github.com>
2022-10-05 09:52:18 +0200

    passed all tests

--- Project/03_coffee_shop_full_stack/starter_code/backend/src/api.py
+++ Project/03_coffee_shop_full_stack/starter_code/backend/src/api.py
@@ -57,58 +57,59 @@ def create_drink(payload):
     body = request.get_json()
 
     try:
-
-        title = body['title']
-        recipe = body['recipe']
-
-        drink = Drink(title=title, recipe=recipe)
+        drink_name = body.get('title', None)
+        drink_recipe = json.dumps(body.get('recipe', None))
+        drink = Drink(title=drink_name, recipe=drink_recipe)
         drink.insert()
 
+        drink = drink.long()
+
         return jsonify({
             'success': True,
-            'drinks': [drink.long()]
-        })
-
-    except Exception:
+            'drinks': drink
+        }), 200
+    except:
         abort(422)
 
 
-@app.route('/drinks/<int:drink_id>', methods=['PATCH'])
+@app.route('/drinks/<int:id>', methods=['PATCH'])
 @requires_auth('patch:drinks')
-def update_drink(drink_id, payload):
-    json_body = request.get_json()
-    drink = Drink.query.filter(Drink.id == drink_id).one_or_none()
+def edit_drink(jwt, id):
+    body = request.get_json()
+    drink = Drink.query.filter(Drink.id == id).one_or_none()
 
     if not drink:
         abort(404)
-
     try:
-        drink.title = json_body['title']
-        drink.recipe = json_body['recipe']
+        req_title = body.get('title', None)
+        req_recipe = json.dumps(body.get('recipe', None))
+        drink.title = req_title
+        drink.recipe = req_recipe
         drink.update()
+        drinks = []
+        drinks.append(drink.long())
 
         return jsonify({
             'success': True,
-            'drinks': [drink.long()]
-        })
-    except Exception:
+            'drinks': drinks
+        }), 200
+    except:
         abort(422)
 
 
-@app.route('/drinks/<int:drink_id>', methods=['DELETE'])
+@app.route('/drinks/<int:id>', methods=['DELETE'])
 @requires_auth('delete:drinks')
-def delete_drink(drink_id, payload):
-    drink = Drink.query.filter(Drink.id == drink_id)
-    if not drink:
-        abort(404)
-    try:
+def delete_drink(jwt, id):
+    drink = Drink.query.get(id)
+    if drink:
         drink.delete()
+
         return jsonify({
             'success': True,
-            'delete': drink.id
-        })
-    except Exception:
-        abort(422)
+            'delete': id
+        }), 200
+    else:
+        abort(404)
 
 
 @app.errorhandler(422)
--- Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -1,14 +1,10 @@
-/* @TODO replace with your variables
- * ensure all variables on this page match your project
- */
-
 export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:9000', // the base url of the running ionic application. 
+    url: 'coffeeappauth.us', // the auth0 domain prefix
+    audience: 'coffee', // the audience set for the auth0 app
+    clientId: 'Q8MtCGRfANDLRWLlwB3FYTw3FNmLIF9M', // the client id generated for the auth0 app
+    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
